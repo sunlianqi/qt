d92d709d9ed966e3b088e29620f553931f9c1225
sunlianqi <lianqsun@163.com>
2021-03-14 22:46:07 +0800

    network

---
+++ network/widget.cpp
@@ -0,0 +1,37 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QNetworkRequest>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    m_network = new QNetworkAccessManager(this);
+    ui->lineEdit->setText("https://www.baidu.com");
+    m_reply = nullptr;
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    m_reply =  m_network->get(QNetworkRequest(QUrl(ui->lineEdit->text())));
+    connect(m_reply,&QNetworkReply::finished,this,&Widget::finishedSlot);
+    connect(m_reply,&QNetworkReply::readyRead,this,&Widget::readSlot);
+}
+
+void Widget::finishedSlot()
+{
+    m_reply->deleteLater();
+    m_reply = nullptr;
+    qDebug() << "reply finished";
+}
+
+void Widget::readSlot()
+{
+    qDebug() << m_reply->readAll();
+}
---
+++ network/widget.h
@@ -0,0 +1,29 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+private slots:
+    void on_pushButton_clicked();
+    void finishedSlot();
+    void readSlot();
+private:
+    QNetworkAccessManager *m_network;
+    QNetworkReply *m_reply;
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
